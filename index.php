1a676ef2879d7b427c4f1705b2b12d11b569e4fe
Самир <s.gahramanov@intensa.ru>
2023-06-30 01:19:35 +0400

    csv parser for magento2

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 declare(strict_types=1);
 
 include_once __DIR__ . '/vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 use Octopus\ParserCsv\Classes\CSVParser;
 
--- src/Classes/CSVParser.php
+++ src/Classes/CSVParser.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Octopus\ParserCsv\Classes;
 
-use  \Exception;
+use \Exception;
 
 class CSVParser
 {
@@ -65,46 +65,17 @@ class CSVParser
         }
     }
 
-    private function prepareSimple(array $product): array
+    private function prepareSimple(array $product): ?array
     {
-        $name = mb_strtoupper($product[0]);
+        $name = $product[0];
         $categories = $product[1];
         $price = $product[4];
-        $nameWithoutSpace = str_replace(" ", '', $name);
-        //Размеры SS
-        $reg = "/[S]{2}[0-9]{2}/";
-        $result = preg_match($reg, $nameWithoutSpace, $found);
+        $nameWithoutSpace = str_replace(' ', '', $name);
 
-        if ($result === 1) {
-            $this->productsNames[] = preg_replace("/\s[S]{2}(\s)?[0-9]{2}/", '', $name);
-            $size = preg_replace("/[S]{2}/", 'SS ', $found[0]);
-            //Размеры без SS
-        } else {
-            $nameWithoutZero = str_replace(",0", '', $name);
-            $nameWithoutSpace = str_replace(" ", '', $nameWithoutZero);
-            //Здесь поменял на точки
-            $reg = "/[0-9]{1,2}(.)?([0-9]{1})?X[0-9]{1,2}(.)?([0-9]{1})?/";
-            $result = preg_match($reg, $nameWithoutSpace, $found);
-            //избавляемся от буквы в аттрибуте размера в конце
-            $found[0] = preg_replace("/X/", 'x', $found[0]);
-            $found[0] = preg_replace("/[A-Z]/", '', $found[0]);
-            $size = strtolower($found[0] . ' mm');
+        $size = $this->extractSize($nameWithoutSpace, $name);
 
-            if ($result === 1) {
-                //Здесь поменял на точки
-                $this->productsNames[] = preg_replace("/\s[0-9]{1,2}(.)?([0-9]{1})?X(\s)?[0-9]{1,2}(.)?([0-9]{1})?/", '', $nameWithoutZero);
-                $name = str_replace(",", '.', $name);
-            } else {
-                $reg = "/[A-Z][0-9]{2}[A-Z]/";
-                $result = preg_match($reg, $nameWithoutSpace, $found);
-                $size = preg_replace("/[A-Z]/", '', $found[0]) . ' mm';
-
-                if ($result === 1) {
-                    $this->productsNames[] = preg_replace("/\s[0-9]{2}/", '', $nameWithoutZero);
-                    $name = str_replace(",", '.', $name);
-                }
-            }
-        }
+        $name = strtoupper(trim($name));
+        $size = str_replace("  ", " ", $size);
 
         return [
             $name, '', 'Default', 'simple', $categories, 'base', $name, '', '', '', '1', 'Taxable Goods', 'Not Visible Individually', $price,
@@ -114,29 +85,142 @@ class CSVParser
         ];
     }
 
+    private function extractSize(string $nameWithoutSpace, string &$name): string
+    {
+        $size = '';
+
+        if ($this->extractSizeWithSS($nameWithoutSpace, $name, $size)) {
+            return $size;
+        }
+
+        $nameWithoutZero = str_replace(".0", '', $name);
+        $nameWithoutZero = str_replace("X ", 'X', $nameWithoutZero);
+        $nameWithoutSpace = str_replace(" ", '', $nameWithoutZero);
+
+        if ($this->extractSizeWithX($nameWithoutZero, $size, $name)) {
+            return $size;
+        }
+
+        if ($this->extractSizeWithTextNumbers($nameWithoutSpace, $nameWithoutZero, $size)) {
+            return $size;
+        }
+
+        if ($this->extractSizeWithDecimal($name, $nameWithoutZero, $size, $nameWithoutSpace)) {
+            return $size;
+        }
+
+        $pattern = '/\s(\d{2})\s/';
+        if (preg_match($pattern, $name, $matches) === 1) {
+            $size = trim('SS' . $matches[0]);
+            $updatedName = preg_replace($pattern, ' ', $nameWithoutZero);
+            $this->productsNames[] = trim(str_replace('  ', ' ', trim($updatedName)));
+        }
+
+        return $size;
+    }
+
+    private function extractSizeWithSS(string $nameWithoutSpace, string &$name, string &$size): bool
+    {
+        $reg = "/[S]{2}[0-9]{2}/";
+        $result = preg_match($reg, $nameWithoutSpace, $found);
+
+        if ($result === 1) {
+            $this->productsNames[] = str_replace('  ', ' ', trim(preg_replace("/\s[S]{2}(\s)?[0-9]{2}/", '', $name)));
+            $size = preg_replace("/[S]{2}/", 'SS ', $found[0]);
+            return true;
+        }
+
+        return false;
+    }
+
+    private function extractSizeWithX(string $nameWithoutZero, string &$size, string &$name): bool
+    {
+        $reg = "/[0-9]{1,2}(.)?([0-9]{1})?X[0-9]{1,2}(.)?([0-9]{1})?/";
+        $result = preg_match($reg, $nameWithoutZero, $found);
+
+        if ($result === 1) {
+            $found[0] = preg_replace("/X/", 'x', $found[0]);
+            $found[0] = preg_replace("/[A-Z]/", '', $found[0]);
+            $found[0] = preg_replace("/^\d{1,2}\s/", '', $found[0]);
+
+            $size = strtolower($found[0] . ' mm');
+            $name = str_replace("X ", 'X', $nameWithoutZero);
+            $this->productsNames[] = trim(str_replace('  ', ' ', (trim(preg_replace("/\s[0-9]{1,2}(.)?([0-9]{1})?X(\s)?[0-9]{1,2}(.)?([0-9]{1})?/", ' ', $nameWithoutZero)))));
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function extractSizeWithTextNumbers(string $nameWithoutSpace, string $nameWithoutZero, string &$size): bool
+    {
+        $reg = "/[A-Z][0-9]{2}[A-Z]/";
+        $result = preg_match($reg, $nameWithoutSpace, $found);
+
+        if ($result === 1) {
+            $size = preg_replace("/[A-Z]/", '', $found[0]) . ' mm';
+            $this->productsNames[] = trim(str_replace('  ', ' ', trim(preg_replace("/\s[0-9]{2}/", '', $nameWithoutZero))));
+            return true;
+        }
+
+        return false;
+    }
+
+    private function extractSizeWithDecimal(string $name, string $nameWithoutZero, string &$size, string $nameWithoutSpace): bool
+    {
+        $pattern = '/\d+\.\d+/';
+        if (preg_match($pattern, $name, $matches) === 1) {
+            $size = str_replace(".0", '', $matches[0]) . ' mm';
+            $number = preg_replace("/\s[0-9]{1,2}(?!X)/", '', $nameWithoutZero);
+            $pattern = '/(\.\d+)?/';
+            $updatedName = preg_replace($pattern, '', $number);
+            $name = str_replace("X ", 'X', $nameWithoutZero);
+            $this->productsNames[] = trim(str_replace('  ', ' ', trim($updatedName)));
+
+            return true;
+        }
+
+        return false;
+    }
+
     private function prepareBundle(string $bundleName, array $productArr): array
     {
         $bundleProdAttributes = [];
         $category = $productArr[0][1];
+
         foreach ($productArr as $productData) {
             self::$bundleItemsCount++;
+            $name = $productData[0];
+            $name = str_replace(",", ".", $name);
+            $name = str_replace("X ", "X", $name);
+            $name = str_replace(".0", "", $name);
+            $sku = strtoupper($name);
 
-            $name = mb_strtoupper($productData[0]);
-            $name = str_replace(",", '.', $name);
+            // Большой пакет и цена
             $pack = $productData[2];
             $packPrice = $productData[4];
-            $smallPack = $productData[3];
-            $smallPackPrice = $productData[5];
+
+            // Маленький пакет и цена
+            if (isset($productData[3]) && isset($productData[5])) {
+                $smallPack = $productData[3];
+                $smallPackPrice = $productData[5];
+            }
 
             if (!empty($pack) && !empty($packPrice)) {
-                $bundleProdAttributes[] = "name=Pack ${pack},type=checkbox,required=0,sku=${name},price=${packPrice},default=0,default_qty=${pack},price_type=fixed,can_change_qty=0";
+                $bundleProdAttributes[] = "name=Pack ${pack},type=checkbox,required=0,sku=${sku},price=${packPrice},default=0,default_qty=${pack},price_type=fixed,can_change_qty=0";
             }
 
             if (!empty($smallPack) && !empty($smallPackPrice)) {
-                $bundleProdAttributes[] = "name=Pack ${smallPack},type=checkbox,required=0,sku=${name},price=${smallPackPrice},default=0,default_qty=${smallPack},price_type=fixed,can_change_qty=0";
+                $bundleProdAttributes[] = "name=Pack ${smallPack},type=checkbox,required=0,sku=${sku},price=${smallPackPrice},default=0,default_qty=${smallPack},price_type=fixed,can_change_qty=0";
             }
         }
+
         $bundleProdAttributes = implode("|", $bundleProdAttributes);
+        $reg = "/(\d{4})(\D+)/";
+
+        $bundleName = preg_replace($reg, "$1 $2", $bundleName);
+        $bundleName = strtoupper(str_replace("  ", " ", $bundleName));
 
         return [
             $bundleName, '', 'Rhinestones', 'bundle', $category, 'base', $bundleName, '', '', '', '1', 'Taxable Goods', 'Catalog, Search', '0.00',
@@ -146,62 +230,22 @@ class CSVParser
         ];
     }
 
-    private function isSuitable(string $bundleName, array $product)
-    {
-        $name = mb_strtoupper($product[0]);
-        $reg = "/\s[S]{2}(\s)?[0-9]{2}/";
-        $result = preg_match($reg, $name);
-
-        if ($result === 1) {
-            $name = preg_replace("/\s[S]{2}(\s)?[0-9]{2}/", '', $name);
-
-            if ($bundleName === $name) {
-                return true;
-            }
-        } else {
-            $name = str_replace(",0", '', $name);
-            $reg = "/\s[0-9]{1,2}(.)?([0-9]{1})?X(\s)?[0-9]{1,2}(.)?([0-9]{1})?/";
-            $result = preg_match($reg, $name);
-
-            if ($result === 1) {
-                $name = preg_replace($reg, '', $name);
-
-                if ($bundleName === $name) {
-                    return true;
-                }
-            } else {
-                $reg = "/\s[0-9]{2}/";
-                $result = preg_match($reg, $name);
-
-                if ($result === 1) {
-                    $name = preg_replace($reg, '', $name);
-
-                    if ($bundleName === $name) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return "Ошибка не прошел проверку на регулярные выражения";
-    }
-
-    /**
-     * @return string|void
-     */
     public function createBundleProduct()
     {
+
         if ($this->fileStart) {
             $bundleProductsList = $this->getBundleProductsList();
             $relatedProducts = [];
             $bundleArr = [];
+
             foreach ($bundleProductsList as $bundleName) {
                 foreach ($this->productsData as $product) {
                     if ($this->isSuitable($bundleName, $product) === true) {
                         $relatedProducts[$bundleName][] = $product;
                     }
                 }
-            }
+            };
+
             foreach ($relatedProducts as $bundleName => $data) {
                 $bundleArr[] = $this->prepareBundle($bundleName, $data);
             }
@@ -213,6 +257,137 @@ class CSVParser
                 return $exception->getMessage();
             }
         }
+    }
+
+    private function isSuitable(string $bundleName, array $product): bool
+    {
+        $name = $product[0];
+        $productName = $bundleName;
+
+        if ($this->hasExactMatch($name, $productName)) {
+            return true;
+        }
+
+        if ($this->hasSizeSSFormat($name, $productName)) {
+            return true;
+        }
+
+        if ($this->hasSizeXFormat($name, $productName)) {
+            return true;
+        }
+
+        if ($this->hasTwoDigitNumberFormat($name, $productName)) {
+            return true;
+        }
+
+        if ($this->hasDecimalNumberFormat($name, $productName)) {
+            return true;
+        }
+
+        if ($this->hasSingleDigitFormat($name, $productName)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function hasExactMatch(string $name, string $productName): bool
+    {
+        return $name === $productName;
+    }
+
+    private function hasSizeSSFormat(string $name, string $productName): bool
+    {
+        $regex = "/\s[S]{2}(\s)?[0-9]{2}/";
+        $result = preg_match($regex, $name);
+
+        if ($result === 1) {
+            $name = str_replace('  ', ' ', trim(preg_replace($regex, '', $name)));
+
+            if ($name === $productName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function hasSizeXFormat(string $name, string $productName): bool
+    {
+        $regex = "/\s[0-9]{1,2}(.)?([0-9]{1})?X(\s)?[0-9]{1,2}(.)?([0-9]{1})?/";
+        $result = preg_match($regex, $name);
+
+        if ($result === 1) {
+            $name = str_replace('  ', ' ', trim(preg_replace($regex, ' ', $name)));
+
+            if ($name === $productName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function hasTwoDigitNumberFormat(string $name, string $productName): bool
+    {
+        $regex = "/\s(\d{2})\s/";
+        $result = preg_match($regex, $name);
+
+        if ($result === 1) {
+            $name = str_replace('  ', '', trim(preg_replace($regex, ' ', $name)));
+
+            if ($name === $productName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function hasDecimalNumberFormat(string $name, string $productName): bool
+    {
+        $pattern = '/\d+\.\d+/';
+
+        if (preg_match($pattern, $name) === 1) {
+            $number = preg_replace("/\s[0-9]{1,2}(?!X)/", '', $name);
+            $pattern = '/(\.\d+)?/';
+            $updatedName = preg_replace($pattern, '', $number);
+            $updatedName = str_replace('  ', '', str_replace("X ", 'X', $updatedName));
+
+            if ($updatedName === $productName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function hasSingleDigitFormat(string $name, string $productName): bool
+    {
+        $pattern = '/\b\d\b/';
+
+        if (preg_match($pattern, $name)) {
+            $updatedName = str_replace('  ', '', preg_replace($pattern, '', $name));
+            $nameArray = str_split($updatedName);
+            $productNameArray = str_split($productName);
+
+            $nameArray = array_filter($nameArray, function ($value) {
+                return !empty($value) && $value != ' ';
+            });
+
+            $productNameArray = array_filter($productNameArray, function ($value) {
+                return !empty($value) && $value != ' ';
+            });
+
+            $name = implode($nameArray);
+            $productName = implode($productNameArray);
+
+            if ($name === $productName) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private function getBundleProductsList(): array
@@ -244,5 +419,4 @@ class CSVParser
         fclose($this->fileStart);
         fclose($this->fileFinish);
     }
-
 }
